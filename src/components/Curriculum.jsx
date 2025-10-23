898110dd3e6a0b1422bf8cbdf55a8016673d8ebf
ridermanriquecueto <ridermc@gmail.com>
2025-10-23 12:05:53 -0300

    Actualización de portfolio

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -7,7 +7,7 @@ export default function Curriculum(){
       <article className="curriculum">
         <h2>¿Quieres descargar mi currículum? Haz clic abajo</h2>
         <a
-          href="https://drive.google.com/file/d/1p_MK52xN1Z0L3PezKLe9mEt_fcMG_-U6/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1dgaRU1YL04PSF2hMvDDK59RT03e4Esvm/view?usp=drive_link"
           aria-label="Descargar currículum"
           target="_blank"
           className="btn"
